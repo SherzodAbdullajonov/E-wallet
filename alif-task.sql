b38f2d00ffc3f0e77a74bfdd79be5114696e571b
SherzodAbdullajonov <sabdullajonov77@gmail.com>
2022-06-04 20:16:08 +0500

    sql file added

---
+++ alif-task.sql
@@ -0,0 +1,179 @@
+CREATE TABLE "acounts" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" int,
+  "balance" float,
+  "identified" boolean,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (1, 1, 81705, true, '10/24/2021', '8/23/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (2, 2, 88991, true, '8/7/2021', '1/17/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (3, 3, 32131, true, '5/5/2022', '3/26/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (4, 4, 40772, true, '8/15/2021', '12/9/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (5, 5, 1296, false, '7/20/2021', '8/10/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (6, 6, 3650, true, '12/31/2021', '10/31/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (7, 7, 6519, false, '3/15/2022', '4/16/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (8, 8, 4154, false, '1/22/2022', '2/23/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (9, 9, 8820, false, '10/5/2021', '8/18/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (10, 10, 16202, true, '12/3/2021', '10/20/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (11, 11, 8900, false, '11/28/2021', '5/11/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (12, 12, 13432, true, '9/22/2021', '12/15/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (13, 13, 16209, true, '12/8/2021', '10/17/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (14, 14, 44858, true, '9/7/2021', '9/9/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (15, 15, 7360, false, '11/5/2021', '4/19/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (16, 16, 5575, false, '1/7/2022', '12/14/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (17, 17, 3061, false, '4/17/2022', '2/22/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (18, 18, 2938, false, '3/28/2022', '6/2/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (19, 19, 79000, true, '8/11/2021', '3/1/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (20, 20, 8000, false, '4/5/2022', '10/21/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (21, 21, 60840, true, '7/11/2021', '1/1/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (22, 22, 78442, true, '9/5/2021', '3/31/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (23, 23, 88307, true, '10/18/2021', '5/10/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (24, 24, 66068, true, '8/16/2021', '1/26/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (25, 25, 47551, true, '8/17/2021', '3/7/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (26, 26, 57040, true, '8/10/2021', '4/13/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (27, 27, 9450, false, '10/11/2021', '10/17/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (28, 28, 92060, true, '8/14/2021', '11/10/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (29, 29, 5450, false, '4/20/2022', '11/24/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (30, 30, 9147, false, '1/27/2022', '9/21/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (31, 31, 9275, true, '2/7/2022', '11/16/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (32, 32, 4570, false, '4/19/2022', '9/5/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (33, 33, 870, true, '5/25/2022', '8/26/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (34, 34, 81740, true, '5/10/2022', '11/2/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (35, 35, 79756, true, '5/29/2022', '5/5/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (36, 36, 1355, false, '8/15/2021', '5/25/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (37, 37, 9610, false, '7/26/2021', '2/8/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (38, 38, 25304, true, '11/5/2021', '9/19/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (39, 39, 69651, true, '7/26/2021', '11/18/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (40, 40, 21857, true, '2/16/2022', '2/24/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (41, 41, 1177, false, '10/5/2021', '5/9/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (42, 42, 45610, true, '10/23/2021', '11/15/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (43, 43, 9706, false, '5/10/2022', '10/21/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (44, 44, 28410, true, '5/26/2022', '2/13/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (45, 45, 81519, true, '11/7/2021', '3/11/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (46, 46, 9280, false, '5/6/2022', '12/1/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (47, 47, 1070, false, '2/20/2022', '12/17/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (48, 48, 6332, true, '5/9/2022', '10/4/2021');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (49, 49, 30789, true, '7/12/2021', '5/17/2022');
+insert into acounts (id, user_id, balance, indentified, created_at, updated_at) values (50, 50, 25130, true, '9/19/2021', '3/21/2022');
+
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+insert into users(id, name, created_at, updated_at) values (1, 'Owen', '6/17/2021', '1/28/2022');
+insert into users (id, name, created_at, updated_at) values (2, 'Nick', '4/17/2022', '4/10/2022');
+insert into users (id, name, created_at, updated_at) values (3, 'Eli', '9/17/2021', '4/5/2022');
+insert into users (id, name, created_at, updated_at) values (4, 'Rhodie', '10/15/2021', '3/1/2022');
+insert into users (id, name, created_at, updated_at) values (5, 'Edmund', '2/2/2022', '5/29/2022');
+insert into users (id, name, created_at, updated_at) values (6, 'Christye', '12/4/2021', '6/10/2021');
+insert into users (id, name, created_at, updated_at) values (7, 'Kass', '8/7/2021', '5/24/2022');
+insert into users (id, name, created_at, updated_at) values (8, 'Valentijn', '12/18/2021', '10/13/2021');
+insert into users(id, name, created_at, updated_at) values (9, 'Pincas', '8/4/2021', '2/11/2022');
+insert into users (id, name, created_at, updated_at) values (10, 'Kenton', '10/8/2021', '1/2/2022');
+insert into users (id, name, created_at, updated_at) values (11, 'Ashby', '4/7/2022', '4/13/2022');
+insert into users (id, name, created_at, updated_at) values (12, 'Becca', '11/8/2021', '11/15/2021');
+insert into users (id, name, created_at, updated_at) values (13, 'Aridatha', '3/11/2022', '5/21/2022');
+insert into users (id, name, created_at, updated_at) values (14, 'Launce', '2/15/2022', '11/4/2021');
+insert into users(id, name, created_at, updated_at) values (15, 'Chryste', '5/21/2022', '4/25/2022');
+insert into users (id, name, created_at, updated_at) values (16, 'Giulietta', '12/30/2021', '12/9/2021');
+insert into users (id, name, created_at, updated_at) values (17, 'Carlye', '6/8/2021', '7/22/2021');
+insert into users (id, name, created_at, updated_at) values (18, 'Bret', '7/8/2021', '8/1/2021');
+insert into users (id, name, created_at, updated_at) values (19, 'Stephanie', '4/30/2022', '9/16/2021');
+insert into users (id, name, created_at, updated_at) values (20, 'Reamonn', '8/2/2021', '8/12/2021');
+insert into users (id, name, created_at, updated_at) values (21, 'Cazzie', '7/8/2021', '11/3/2021');
+insert into users (id, name, created_at, updated_at) values (22, 'Cora', '5/1/2022', '5/28/2022');
+insert into users (id, name, created_at, updated_at) values (23, 'Saleem', '8/11/2021', '11/21/2021');
+insert into users (id, name, created_at, updated_at) values (24, 'Mendy', '10/7/2021', '4/27/2022');
+insert into users (id, name, created_at, updated_at) values (25, 'Horatio', '7/9/2021', '3/16/2022');
+insert into users (id, name, created_at, updated_at) values (26, 'Ikey', '5/8/2022', '2/1/2022');
+insert into users (id, name, created_at, updated_at) values (27, 'Pierre', '2/27/2022', '3/23/2022');
+insert into users (id, name, created_at, updated_at) values (28, 'Alaster', '1/15/2022', '4/3/2022');
+insert into users (id, name, created_at, updated_at) values (29, 'Janis', '9/1/2021', '2/4/2022');
+insert into users (id, name, created_at, updated_at) values (30, 'Wilmer', '7/25/2021', '8/19/2021');
+insert into users (id, name, created_at, updated_at) values (31, 'Lura', '11/7/2021', '4/6/2022');
+insert into users(id, name, created_at, updated_at) values (32, 'Karna', '7/14/2021', '12/31/2021');
+insert into users (id, name, created_at, updated_at) values (33, 'Rycca', '5/8/2022', '7/2/2021');
+insert into users (id, name, created_at, updated_at) values (34, 'Tatiania', '10/11/2021', '9/27/2021');
+insert into users (id, name, created_at, updated_at) values (35, 'Gerry', '3/10/2022', '7/16/2021');
+insert into users (id, name, created_at, updated_at) values (36, 'Miguela', '6/26/2021', '7/18/2021');
+insert into users (id, name, created_at, updated_at) values (37, 'Henderson', '12/21/2021', '11/20/2021');
+insert into users (id, name, created_at, updated_at) values (38, 'Bradney', '1/17/2022', '2/20/2022');
+insert into users (id, name, created_at, updated_at) values (39, 'Ruy', '10/2/2021', '7/11/2021');
+insert into users (id, name, created_at, updated_at) values (40, 'Lauritz', '8/28/2021', '7/8/2021');
+insert into users (id, name, created_at, updated_at) values (41, 'Bee', '7/27/2021', '12/2/2021');
+insert into users (id, name, created_at, updated_at) values (42, 'Edmon', '7/24/2021', '4/1/2022');
+insert into users (id, name, created_at, updated_at) values (43, 'Rorie', '2/8/2022', '4/15/2022');
+insert into users (id, name, created_at, updated_at) values (44, 'Kerwin', '12/16/2021', '7/25/2021');
+insert into users (id, name, created_at, updated_at) values (45, 'Amabel', '5/8/2022', '3/25/2022');
+insert into users (id, name, created_at, updated_at) values (46, 'Eliza', '7/11/2021', '1/3/2022');
+insert into users (id, name, created_at, updated_at) values (47, 'Keene', '7/22/2021', '12/19/2021');
+insert into users (id, name, created_at, updated_at) values (48, 'Emalia', '1/20/2022', '10/16/2021');
+insert into users (id, name, created_at, updated_at) values (49, 'Skippie', '7/27/2021', '3/31/2022');
+insert into users (id, name, created_at, updated_at) values (50, 'Sharl', '7/31/2021', '9/14/2021');
+
+CREATE TABLE "topups" (
+  "id" SERIAL PRIMARY KEY,
+  "amount" float,
+  "account_id" int,
+  "created_at" timestamp
+);
+insert into topups (id, amount, account_id, created_at) values (1, 57332, 1, '6/23/2021');
+insert into topups (id, amount, account_id, created_at) values (2, 37185, 2, '11/13/2021');
+insert into topups (id, amount, account_id, created_at) values (3, 41385, 3, '8/25/2021');
+insert into topups (id, amount, account_id, created_at) values (4, 25851, 4, '10/11/2021');
+insert into topups (id, amount, account_id, created_at) values (5, 1755, 5, '8/19/2021');
+insert into topups (id, amount, account_id, created_at) values (6, 79455, 6, '5/21/2022');
+insert into topups (id, amount, account_id, created_at) values (7, 49475, 7, '1/31/2022');
+insert into topups (id, amount, account_id, created_at) values (8, 5440, 8, '6/13/2021');
+insert into topups (id, amount, account_id, created_at) values (9, 64120, 9, '10/29/2021');
+insert into topups (id, amount, account_id, created_at) values (10, 87760, 10, '7/28/2021');
+insert into topups (id, amount, account_id, created_at) values (11, 22072, 11, '10/16/2021');
+insert into topups (id, amount, account_id, created_at) values (12, 26684, 12, '12/11/2021');
+insert into topups (id, amount, account_id, created_at) values (13, 19625, 13, '6/24/2021');
+insert into topups (id, amount, account_id, created_at) values (14, 95593, 14, '7/2/2021');
+insert into topups (id, amount, account_id, created_at) values (15, 73320, 15, '10/27/2021');
+insert into topups (id, amount, account_id, created_at) values (16, 74775, 16, '12/24/2021');
+insert into topups (id, amount, account_id, created_at) values (17, 11678, 17, '3/19/2022');
+insert into topups (id, amount, account_id, created_at) values (18, 22973, 18, '3/4/2022');
+insert into topups (id, amount, account_id, created_at) values (19, 9196, 19, '6/11/2021');
+insert into topups (id, amount, account_id, created_at) values (20, 9443, 20, '7/31/2021');
+insert into topups (id, amount, account_id, created_at) values (21, 44550, 21, '12/4/2021');
+insert into topups (id, amount, account_id, created_at) values (22, 21321, 22, '4/9/2022');
+insert into topups (id, amount, account_id, created_at) values (23, 12581, 23, '3/12/2022');
+insert into topups (id, amount, account_id, created_at) values (24, 94762, 24, '1/31/2022');
+insert into topups (id, amount, account_id, created_at) values (25, 62867, 25, '4/29/2022');
+insert into topups (id, amount, account_id, created_at) values (26, 83492, 26, '1/23/2022');
+insert into topups (id, amount, account_id, created_at) values (27, 1348, 27, '11/8/2021');
+insert into topups (id, amount, account_id, created_at) values (28, 11361, 28, '5/14/2022');
+insert into topups (id, amount, account_id, created_at) values (29, 82379, 29, '1/21/2022');
+insert into topups (id, amount, account_id, created_at) values (30, 69983, 30, '3/26/2022');
+insert into topups (id, amount, account_id, created_at) values (31, 92027, 31, '11/11/2021');
+insert into topups (id, amount, account_id, created_at) values (32, 49177, 32, '8/10/2021');
+insert into topups (id, amount, account_id, created_at) values (33, 44206, 33, '10/10/2021');
+insert into topups (id, amount, account_id, created_at) values (34, 99720, 34, '4/17/2022');
+insert into topups (id, amount, account_id, created_at) values (35, 11382, 35, '1/9/2022');
+insert into topups (id, amount, account_id, created_at) values (36, 95820, 36, '7/13/2021');
+insert into topups (id, amount, account_id, created_at) values (37, 96204, 37, '12/5/2021');
+insert into topups (id, amount, account_id, created_at) values (38, 33101, 38, '6/8/2021');
+insert into topups (id, amount, account_id, created_at) values (39, 33008, 39, '9/25/2021');
+insert into topups (id, amount, account_id, created_at) values (40, 28751, 40, '9/15/2021');
+insert into topups (id, amount, account_id, created_at) values (41, 43871, 41, '7/22/2021');
+insert into topups (id, amount, account_id, created_at) values (42, 19837, 42, '11/23/2021');
+insert into topups (id, amount, account_id, created_at) values (43, 37239, 43, '1/18/2022');
+insert into topups (id, amount, account_id, created_at) values (44, 14466, 44, '12/13/2021');
+insert into topups (id, amount, account_id, created_at) values (45, 55239, 45, '7/6/2021');
+insert into topups (id, amount, account_id, created_at) values (46, 68260, 46, '3/15/2022');
+insert into topups (id, amount, account_id, created_at) values (47, 41496, 47, '3/1/2022');
+insert into topups (id, amount, account_id, created_at) values (48, 16630, 48, '5/27/2022');
+insert into topups (id, amount, account_id, created_at) values (49, 34909, 49, '11/1/2021');
+insert into topups (id, amount, account_id, created_at) values (50, 9057, 50, '4/19/2022');
+
+ALTER TABLE "acounts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "topups" ADD FOREIGN KEY ("id") REFERENCES "acounts" ("id");
+
+
