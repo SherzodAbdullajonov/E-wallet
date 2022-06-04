523248b30a6c399af6fa1b5df980ee73179c2826
SherzodAbdullajonov <sabdullajonov77@gmail.com>
2022-06-04 18:50:49 +0500

    all routers added

--- controller/controller.go
+++ controller/controller.go
@@ -13,24 +13,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetBalance(c *gin.Context) {
-	var _, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-	var balance []models.Accounts
-	defer cancel()
-	if err := database.Db.Find(&balance).Error; err != nil {
-		c.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		c.JSON(200, balance)
+func GetBalance() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var _, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var balance []models.Accounts
+		defer cancel()
+		if err := database.Db.Find(&balance).Error; err != nil {
+			ctx.AbortWithStatus(404)
+			fmt.Println(err)
+		} else {
+			ctx.JSON(200, balance)
+		}
 	}
+
 }
-func TopUp() gin.HandlerFunc{
+func TopUp() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var _, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		var topUp models.TopUps
 		err := ctx.BindJSON(&topUp)
 		defer cancel()
-		if err != nil{
+		if err != nil {
 			ctx.JSON(http.StatusBadRequest, err)
 			return
 		}
@@ -38,25 +41,23 @@ func TopUp() gin.HandlerFunc{
 		database.Db.Create(&topUp)
 		ctx.JSON(http.StatusOK, topUp)
 
-
 	}
 }
 
-func CheckAcount() gin.HandlerFunc{
+func CheckAcount() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var _, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		id := ctx.Params.ByName("id")
 		log.Println("ID", id)
 		var user models.Users
 		defer cancel()
-		if err := database.Db.Where("id=?", id).First(&user).Error; err!=nil{
+		if err := database.Db.Where("id=?", id).First(&user).Error; err != nil {
 			ctx.AbortWithStatus(404)
 			fmt.Println(err)
 		} else {
 			ctx.JSON(http.StatusOK, user)
 		}
 
-		
 	}
 }
 
@@ -67,13 +68,13 @@ func GetTotalTopUps() gin.HandlerFunc {
 		var amount models.Accounts
 		var total models.TopUps
 		if total.Created_at == time.Now().Add(time.Duration(time.Now().Month())) {
-			if err:= database.Db.Find(&total).Error; err!= nil{
+			if err := database.Db.Find(&total).Error; err != nil {
 				ctx.AbortWithStatus(http.StatusNotFound)
 				fmt.Println(err)
 			} else {
 				ctx.JSON(http.StatusOK, total.Amount)
 			}
-			if err:= database.Db.Find(&amount).Error; err!= nil{
+			if err := database.Db.Find(&amount).Error; err != nil {
 				ctx.AbortWithStatus(http.StatusNotFound)
 				fmt.Println(err)
 			} else {
@@ -81,6 +82,5 @@ func GetTotalTopUps() gin.HandlerFunc {
 			}
 		}
 
-		
 	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/SherzodAbdullajonov/e-wallet/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func UseRoutes(incomingRoutes *gin.Engine)  {
+	incomingRoutes.POST("/balance", controller.GetBalance())
+	incomingRoutes.POST("/top_up", controller.GetTotalTopUps())
+	incomingRoutes.POST("/user", controller.CheckAcount())
+	incomingRoutes.POST("/total", controller.GetTotalTopUps())
+}
